535977824b2de12ad7c8aaace85ef4092214f2fb
Isuru-Dissanayake <isurumailme@gmail.com>
2020-12-19 21:13:17 +0530

    Education page cards

--- src/components/pages/educationpage/index.js
+++ src/components/pages/educationpage/index.js
@@ -1,14 +1,45 @@
 import React,{ Component } from 'react';
 import './styles.css'
-import amayaPhoto from '../../../assets/images/amaya.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithubSquare, faMedium, faLinkedin, faYoutubeSquare } from "@fortawesome/free-brands-svg-icons"
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 class EducationPage extends React.Component {
+
+  changeEducationCard = () => {
+    const windowWidth = window.innerWidth;
+    console.log(windowWidth);
+  }
+  componentDidMount = () => {
+    window.addEventListener("resize", this.changeEducationCard);
+  }
+
   render() {
     return(
-      <Element name="Education-page-section" className="Education-page-section"></Element>
+      <Element name="Education-page-section" className="Education-page-section">
+        <div className="education-page-title">
+          <div className="hover-to-tomato">E</div>
+          <div className="hover-to-tomato">d</div>
+          <div className="hover-to-tomato">u</div>
+          <div className="hover-to-tomato">c</div>
+          <div className="hover-to-tomato">a</div>
+          <div className="hover-to-tomato">t</div>
+          <div className="hover-to-tomato">i</div>
+          <div className="hover-to-tomato">o</div>
+          <div className="hover-to-tomato">n</div>
+        </div>
+        <div className="education-page-cards-container">
+          <div className="educatio-page-card">
+
+          </div>
+          <div className="educatio-page-card">
+
+          </div>
+          <div className="educatio-page-card">
+
+          </div>
+        </div>
+      </Element>
     )
   }
 }
