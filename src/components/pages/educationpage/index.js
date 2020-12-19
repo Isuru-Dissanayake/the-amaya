2b4c255df91d271d5ae8eb44ef163080c48cedb1
Isuru-Dissanayake <isurumailme@gmail.com>
2020-12-20 01:15:30 +0530

    card hovering effect

--- src/components/pages/educationpage/index.js
+++ src/components/pages/educationpage/index.js
@@ -46,7 +46,7 @@ class EducationPage extends React.Component {
   render() {
     return(
       
-      <Element id="Education-page-section" name="Education-page-section" className="Education-page-section">
+      <Element id="Education-page-section" name="Education-page-section" className="Education-page-section fade-in">
         <div className="education-page-title">
           <div className="hover-to-tomato">E</div>
           <div className="hover-to-tomato">d</div>
@@ -60,7 +60,7 @@ class EducationPage extends React.Component {
         </div>
         <div id="education-page-cards-container" className="education-page-cards-container">
           <FadeInSection>
-            <div className="educatio-page-card">
+            <div className="educatio-page-card card">
               <FontAwesomeIcon icon={faSchool} className="education-page-card-icon"/>
               <div className="education-page-card-title">Maliyadeva Girls’ College</div>
               <div className="education-page-card-title">G.C.E. Advanced Level</div>
@@ -71,7 +71,7 @@ class EducationPage extends React.Component {
             </div>
           </FadeInSection>
           <FadeInSection>
-            <div className="educatio-page-card">
+            <div className="educatio-page-card card">
               <FontAwesomeIcon icon={faGraduationCap} className="education-page-card-icon"/>
               <div className="education-page-card-title">University of Moratuwa, Sri Lanka</div>
               <div className="education-page-card-title">B.Sc. Engineering</div>
@@ -83,7 +83,7 @@ class EducationPage extends React.Component {
             </div>
           </FadeInSection>
           <FadeInSection>
-            <div className="educatio-page-card">
+            <div className="educatio-page-card card">
               <FontAwesomeIcon icon={faBriefcase} className="education-page-card-icon"/>
               <div className="education-page-card-title">University of Sydney</div>
               <div className="education-page-card-title">Visiting Student Researcher</div>
